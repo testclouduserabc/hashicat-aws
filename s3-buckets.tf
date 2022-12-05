ecadc77900ff94dfe20b3f59903cf36f0ba6507e
test user <testclouduser.abc@gmail.com>
2022-12-05 11:16:27 +0000

    added s3 buckets

--- s3-buckets.tf
+++ s3-buckets.tf
@@ -2,8 +2,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/kamranabid/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = "kamran-bucket-7284782783285728375"
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-8738758723572875872737568236752638"
   acl    = "private"
 
   versioning = {
